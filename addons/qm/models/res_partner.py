f15caa95a170110f7dce0ec83a9c25b1484c3449
aladdinwang <aladdin.wang@gmail.com>
2020-08-16 23:38:00 +0800

    customer and supplier

--- addons/qm/models/res_partner.py
+++ addons/qm/models/res_partner.py
@@ -8,3 +8,17 @@ class Partner(models.Model):
     account_address = fields.Char("Account Street")
     account_phone = fields.Char("Account Phone")
     account_name = fields.Char("Account Name")
+
+    supplier_type = fields.Selection(
+        string="Supplier Type",
+        selection=[("agent", "Agent"), ("dealer", "Dealer"), ("producer", "Producer")],
+    )
+    customer_type = fields.Selection(
+        string="Customer Type",
+        selection=[
+            ("person", "Person"),
+            ("dealer", "Dealer"),
+            ("platform", "Platform"),
+            ("endpoint", "Endpoint"),
+        ],
+    )
