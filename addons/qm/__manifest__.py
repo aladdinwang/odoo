ceb3c9227cdf4bae42933217b873e2902e12f530
aladdinwang <aladdin.wang@gmail.com>
2020-08-19 00:24:50 +0800

    product sequence

--- addons/qm/__manifest__.py
+++ addons/qm/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["base", "mail", "uom", "account", "l10n_cn_standard"],
     "description": "专为荃玟定制",
     "data": [
+        "data/ir_sequence_data.xml",
         "data/l10n_cn_chart_data.xml",
         "data/account_tax_template_data.xml",
         "data/account_chart_template_data.xml",
--- addons/qm/models/product.py
+++ addons/qm/models/product.py
@@ -1,8 +1,44 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
     code = fields.Char("Reference")
+    complete_code = fields.Char(
+        "Complete Code", compute="_compute_complete_code", store=True
+    )
     tax_classification_id = fields.Many2one("tax.classification", "Tax Classification")
+
+    @api.depends("code", "parent_id.code", "parent_id.complete_name")
+    def _compute_complete_code(self):
+        for category in self:
+            if category.parent_id:
+                category.complete_code = "".join(
+                    filter(None, [category.parent_id.complete_code, category.code])
+                )
+            else:
+                category.complete_code = category.code
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    default_code = fields.Char(
+        "Internal Reference", compute="_compute_default_code", store=True, index=True
+    )
+
+    @api.depends("product_tmpl_id.categ_id")
+    def _compute_default_code(self):
+        for product in self:
+            if product.product_tmpl_id.categ_id:
+                product.default_code = "".join(
+                    filter(
+                        None,
+                        [
+                            product.product_tmpl_id.categ_id.complete_code,
+                            self.env["ir.sequence"].next_by_code("product.product"),
+                        ],
+                    )
+                )
+                print(product.default_code)
