da8af06c9a5376c0ac6909d6764397af7c8e60a7
aladdinwang <aladdin.wang@gmail.com>
2020-09-08 10:29:18 +0800

    update script

--- addons/qm/scripts/backup.sh
+++ addons/qm/scripts/backup.sh
@@ -4,9 +4,9 @@ cur_time=$(date '+%Y-%m-%d')
 sevendays_time=$(date -d -7days '+%Y-%m-%d')
 
 pushd /root/backup
-rm -rf vgroups.*.dmp
+rm -rf vgroups.*.sql
 rm -rf vgroups.$sevendays_time.tar.gz
-pg_dump -h localhost -U vgroups vgroups > vgroups.$cur_time.dmp
-tar zcvf vgroups.$cur_time.tar.gz *.dmp
-rm -rf vgroups.*.dmp
+pg_dump -h localhost -U vgroups vgroups > vgroups.$cur_time.sql
+tar zcvf vgroups.$cur_time.tar.gz *.sql
+rm -rf vgroups.*.sql
 popd
