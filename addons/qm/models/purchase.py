1318dc157d0c65df44bbfe8037a46797a85fea19
aladdinwang <aladdin.wang@gmail.com>
2021-01-15 18:28:13 +0800

    create purchase order

--- addons/qm/models/purchase.py
+++ addons/qm/models/purchase.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import datetime
+from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
@@ -60,6 +61,177 @@ class PurchaseOrder(models.Model):
             else:
                 order.payment_state = "not_paid"
 
+    @api.model
+    def _get_picking_type(self, company_id):
+        picking_type = self.env["stock.picking.type"].search(
+            [("code", "=", "incoming"), ("warehouse_id.company_id", "=", company_id.id)]
+        )
+        if not picking_type:
+            picking_type = self.env["stock.picking.type"].search(
+                [("code", "=", "incoming"), ("warehouse_id", "=", False)]
+            )
+        return picking_type[:1]
+
+    @api.model
+    def default_get(self, default_fields):
+        rec = super().default_get(default_fields)
+        active_ids = self._context.get("active_ids") or self._context.get("active_id")
+        active_model = self._context.get("active_model")
+
+        if not active_ids or active_model != "purchase.request":
+            return rec
+
+        # 创建默认采购单
+        reqs = (
+            self.env["purchase.request"]
+            .browse(active_ids)
+            .filtered(lambda x: x.state == "open")
+        )
+        if not reqs:
+            raise UserError(_("You can only select open request"))
+
+        partner_id = reqs[0].partner_id
+        partner = self.env["res.partner"].browse(partner_id)
+        is_dropshipping = reqs[0].is_dropshipping
+
+        if is_dropshipping:
+            picking_type_id = self.env["stock.picking.type"].search(
+                [
+                    ("sequence_code", "=", "DS"),
+                    "|",
+                    ("warehouse_id", "=", False),
+                    ("warehouse_id.company_id", "=", company_id.id),
+                ],
+                limit=1,
+            )
+        else:
+            picking_type_id = self.env["stock.picking.type"].search(
+                [
+                    ("sequence_code", "=", "OUT"),
+                    "|",
+                    ("warehouse_id", "=", False),
+                    ("warehouse_id.company_id", "=", company_id.id),
+                ],
+                limit=1,
+            )
+
+        fpos = (
+            self.env["account.fiscal.position"]
+            .with_context(force_company=self.company_id.id)
+            .get_fiscal_position(partner.id)
+        )
+        currency_id = (
+            partner.with_context(
+                force_company=self.company_id.id
+            ).property_purchase_currency_id.id
+            or self.company_id.currency_id.id
+        )
+
+        origins = set()
+        new_order_lines = []
+        now = datetime.datetime.now()
+        for req in reqs:
+            origins.add(req.sale_order_id.name)
+            product_id = req.product_id
+            uom_po_qty = req.product_uom._compute_quantity(
+                req.product_uom_qty, product_id.uom_po_id
+            )
+            seller = product_id.with_context(
+                force_company=self.company_id.id
+            )._select_seller(
+                partner_id=partner,
+                quantity=uom_po_qty,
+                date=now,
+                uom_id=product_id.uom_po_id,
+            )
+            taxes = product_id.supplier_taxes_id
+            # fpos = po.fiscal_position_id
+            taxes_id = fpos.map_tax(taxes, product_id, seller.name) if fpos else taxes
+            if taxes_id:
+                taxes_id = taxes_id.filtered(
+                    lambda x: x.company_id.id == self.company_id.id
+                )
+
+            price_unit = (
+                self.env["account.tax"]._fix_tax_included_price_company(
+                    seller.price,
+                    product_id.supplier_taxes_id,
+                    taxes_id,
+                    self.company_id,
+                )
+                if seller
+                else 0.0
+            )
+            if (
+                price_unit
+                and seller
+                and currency_id
+                and seller.currency_id != currency_id
+            ):
+                price_unit = seller.currency_id._convert(
+                    price_unit, currency_id, self.company_id, fields.Date.today()
+                )
+
+            product_lang = product_id.with_prefetch().with_context(
+                lang=partner.lang, partner_id=partner.id
+            )
+            name = product_lang.display_name
+            if product_lang.description_purchase:
+                name += "\n" + product_lang.description_purchase
+
+            date_planned = datetime.today() + relativedelta(
+                days=seller.delay if seller else 0
+            )
+            new_order_lines.append(
+                (
+                    0,
+                    0,
+                    {
+                        "name": name,
+                        "product_qty": uom_po_qty,
+                        "product_id": product_id.id,
+                        "product_uom": product_id.uom_po_id.id,
+                        "price_unit": price_unit,
+                        "date_planned": date_planned,
+                        "taxes_id": [(6, 0, taxes_id.ids)],
+                    },
+                )
+            )
+
+        order_vals = {
+            "partner_id": partner.id,
+            "user_id": self.user_id.id or False,
+            "picking_type_id": picking_type_id.id,
+            "company_id": self.company_id.id,
+            "currency_id": currency_id,
+            "dest_address_id": picking_type_id.default_location_dest_id.id,
+            "origin": ",".join(sorted(origins)),
+            "payment_term_id": partner.with_context(
+                force_company=self.company_id.id
+            ).property_supplier_payment_term_id.id,
+            "fiscal_position_id": fpos,
+            "date_order": datetime.datetime.now(),
+            "group_id": False,
+            "order_line": new_order_lines,
+        }
+        rec.update(order_vals)
+        return rec
+
+    def action_create_purchase_order(self):
+        active_ids = self.env.context.get("active_ids")
+        if not active_ids:
+            return ""
+
+        return {
+            "name": _("Create Purchase Order"),
+            "res_model": "purchase.order",
+            "view_mode": "form",
+            "view_id": self.env.ref("qm.view_purchase_order_form_qm"),
+            "context": self.env.context,
+            "target": "new",
+            "type": "ir.actions.act_window",
+        }
+
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
@@ -99,6 +271,12 @@ class PurchaseRequest(models.Model):
     )
     customer_id = fields.Many2one(
         "res.partner", related="sale_order_id.partner_id", readonly=True, store=True
+    )
+    customer_shipping_id = fields.Many2one(
+        "res.partner",
+        related="sale_order_id.partner_shipping_id",
+        readonly=True,
+        store=True,
     )
     # delivery_type = fields.Selection(
     #     related="sale_order_id.delivery_type", readonly=True
@@ -192,7 +370,7 @@ class PurchaseRequest(models.Model):
     @api.model
     def _get_picking_type(self, company_id):
         picking_type = self.env["stock.picking.type"].search(
-            [("code", "=", "incoming"), ("warehouse_id.company_id", "=", company_id)]
+            [("code", "=", "incoming"), ("warehouse_id.company_id", "=", company_id.id)]
         )
         if not picking_type:
             picking_type = self.env["stock.picking.type"].search(
@@ -200,35 +378,47 @@ class PurchaseRequest(models.Model):
             )
         return picking_type[:1]
 
-    def action_create_purchase_order(self):
-        partners_by_picking_policy = defaultdict(list)
+    def action_request_create_purchase_order(self):
         origins = set()
+        customer_partner_id_tuples = []
 
+        last_rec = None
         for rec in self:
             origins.add(rec.sale_order_id.name)
-            partners_by_picking_policy[rec.picking_policy].append(
-                (rec.customer_id.id, rec.partner_id.id)
+            customer_partner_id_tuples.append(
+                (rec.customer_shipping_id.id, rec.partner_id.id)
             )
-        if len(partners_by_picking_policy) > 1:
-            raise UserError(_("Only one delivery type at most"))
+            if last_rec and last_rec.is_dropshipping != rec.is_dropshipping:
+                raise UserError(_("Only one delivery type at most"))
+            last_rec = rec
 
-        picking_policy = next(iter(partners_by_picking_policy.keys()))
-        partner_tuples = partners_by_picking_policy[picking_policy]
+        # 如果没有选中, 则什么都不做
+        if last_rec is None:
+            return
 
-        partner_ids = set(x[1] for x in partner_tuples)
+        partner_ids = set(x[1] for x in customer_partner_id_tuples)
         if len(partner_ids) > 1:
             raise UserError(_("Only one supplier at most"))
+
+        if last_rec.is_dropshipping:
+            if len(customer_partner_id_tuples) > 1:
+                raise UserError(_("Only one customer at most"))
+
+        return (
+            self.env["purchase.order"]
+            .with_context(
+                active_ids=self.ids, active_model="purchase.request", active_id=self.id
+            )
+            .action_create_purchase_order()
+        )
+
         partner_id = partner_ids.pop()
 
-        if picking_policy == "dropship":
-            if len(set(x[0] for x in partner_tuples)) > 1:
-                raise UserError(_("Dropship only support one customer"))
-
         # 分离有库存的rec以及无库存的rec, 记录stock_move
-
         company_id = self.company_id
         # picking_type_id = self._get_picking_type(company_id)
         partner = self.env["res.partner"].browse(partner_id)
+
         fpos = (
             self.env["account.fiscal.position"]
             .with_context(force_company=company_id.id)
@@ -253,6 +443,3 @@ class PurchaseRequest(models.Model):
             "date_order": datetime.datetime.now(),
             "group_id": False,
         }
-
-    def _prepare_purchase_order(self, company_id, values):
-        ...
