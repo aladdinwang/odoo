b75746c009c2408715f9be6c5a3f0077b1db251f
root <root@localhost.localdomain>
2021-04-06 10:22:05 -0400

    fix

--- addons/qm/models/account_payment.py
+++ addons/qm/models/account_payment.py
@@ -197,13 +197,33 @@ class SalePaymentRegister(models.Model):
         self.write({"state": "draft"})
 
     def action_confirm(self):
-        self.filtered(lambda x: x.state == "waiting").write({"state": "confirmed"})
+        recs = self.filtered(lambda x: x.state == "waiting")
+        recs.write(
+            {
+                "confirm_by": self.env.user.id,
+                "state": "confirmed",
+                "confirm_date": fields.Date.today(),
+            }
+        )
 
     def action_reconcile(self):
-        self.filtered(lambda x: x.state == "confirmed").write({"state": "reconciled"})
+        recs = self.filtered(lambda x: x.state == "confirmed")
+        recs.write(
+            {
+                "state": "reconciled",
+                "reconciled_by": self.env.user.id,
+                "reconciled_date": fields.Date.today(),
+            }
+        )
 
     def action_cancel(self):
-        self.write({"state": "cancelled"})
+        self.write(
+            {
+                "state": "cancelled",
+                "cancel_by": self.env.user.id,
+                "cancel_date": fields.Date.today(),
+            }
+        )
 
     @api.onchange("amount", "line_ids", "line_ids.amount")
     def _onchange_amount(self):
