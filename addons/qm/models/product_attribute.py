2f547199c907ea94b8ec7aaa4c9cb969db245f50
aladdinwang <aladdin.wang@gmail.com>
2021-05-10 12:17:01 +0800

    fix

--- addons/qm/models/product_attribute.py
+++ addons/qm/models/product_attribute.py
@@ -1,6 +1,22 @@
 from odoo import fields, models, api
 
 
+class ProductAttribute(models.Model):
+    _inherit = "product.attribute"
+
+    comment = fields.Char("Additional Information")
+
+    def name_get(self):
+        self.browse(self.ids).read(["comment"])
+        return [
+            (
+                attr.id,
+                "%s%s" % (attr.name, attr.comment and " [%s]" % attr.comment or ""),
+            )
+            for attr in self
+        ]
+
+
 class ProductTemplateAttributeValue(models.Model):
     _inherit = "product.template.attribute.value"
 
