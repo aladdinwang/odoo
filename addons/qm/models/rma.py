5697980f0ebb920e2c43f0885f739d1b085257ee
aladdin.wang <aladdin.wang@gmail.com>
2021-06-29 11:01:50 +0800

    fix

--- addons/qm/models/rma.py
+++ addons/qm/models/rma.py
@@ -15,24 +15,24 @@ class Rma(models.Model):
         default=lambda self: _("New"),
     )
     type = fields.Selection([("return", "Return"), ("exchange", "Exchange")])
-    sale_order_id = fieldse(
+    sale_order_id = fields.Many2one(
         "sale.order", string="Sale Order", index=True, required=True
     )
     partner_id = fields.Many2one(
         "sale.order", related="sale_order_id.partner_id", index=True, store=True
     )
 
-    return_line_ids = fields.One2many("sale.rma.return.line", "rma_id")
+    return_line_ids = fields.One2many("sale.rma.return_line", "rma_id")
     return_amount = fields.Monetary("Return Amount", required=True)
     exchange_amount = fields.Monetary("Exchange Amount", required=True)
-    exchange_line_ids = fields.One2many("sale.rma.exchange.line", "rma_id")
+    exchange_line_ids = fields.One2many("sale.rma.exchange_line", "rma_id")
 
     # 备注
     comment = fields.Text("Comment")
 
 
 class RmaReturnLine(models.Model):
-    _name = "sale.rma.return.line"
+    _name = "sale.rma.return_line"
     _inherit = ["portal.mixin", "mail.thread", "mail.activity.mixin"]
     _description = "Sale Rma Return Line"
     _order = "create_date desc, name desc, id desc"
@@ -62,7 +62,7 @@ class RmaReturnLine(models.Model):
 
 
 class RmaExchangeLine(models.Model):
-    _name = "sale.rma.exchange.line"
+    _name = "sale.rma.exchange_line"
     _inherit = ["portal.mixin", "mail.thread", "mail.activity.mixin"]
     _description = "Sale Rma Exchange Line"
     _order = "create_date desc, name desc, id desc"
@@ -72,3 +72,19 @@ class RmaExchangeLine(models.Model):
     product_uom_category_id = fields.Many2one(
         "uom.category", compute="_compute_product_uom_category_id"
     )
+    product_qty = fields.Float(
+        string="Quantity", digits="Product Unit Of Measure", required=True, default=1.0
+    )
+    product_uom = fields.Many2one(
+        "uom.uom",
+        string="Unit of Measure",
+        domain="[('category_id', '=', product_uom_category_id)]",
+    )
+
+    price_unit = fields.Float(
+        "Unit Price", required=True, digit="Product Price", default=0.0
+    )
+    price_subtotal = fields.Monetary(
+        compute="_compute_amount", string="Subtotal", readonly=True, store=True)
+
+
