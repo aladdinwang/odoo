e828a3eec156036ba6032c8866de499efe8806bf
aladdinwang <aladdin.wang@gmail.com>
2021-07-05 16:46:15 +0800

    fix

--- addons/qm/models/rma.py
+++ addons/qm/models/rma.py
@@ -12,6 +12,10 @@ class Rma(models.Model):
     def _get_default_currency_id(self):
         return self.env.company.currency_id.id
 
+    @api.depends("return_lines_ids", "exchange_line_ids")
+    def _compute_amount(self):
+        ...
+
     name = fields.Char(
         states={"draft": [("readonly", False)]},
         index=True,
@@ -27,10 +31,10 @@ class Rma(models.Model):
     is_dropshipping = fields.Boolean(related="sale_order_id.is_dropshipping")
 
     return_line_ids = fields.One2many("sale.rma.return_line", "rma_id")
-    return_amount = fields.Monetary("Return Amount")
-    exchange_amount = fields.Monetary("Exchange Amount")
+    return_amount = fields.Monetary("Return Amount", compute="_compute_amount")
+    exchange_amount = fields.Monetary("Exchange Amount", compute="_compute_amount")
     exchange_line_ids = fields.One2many("sale.rma.exchange_line", "rma_id")
-    exchange_diff = fields.Monetary("Exchange diff")
+    exchange_diff = fields.Monetary("Exchange diff", compute="_compute_amount")
     # 备注
     comment = fields.Text("Comment")
 
@@ -43,6 +47,7 @@ class Rma(models.Model):
         ],
         string="Status",
         tracking=True,
+        default="draft",
     )
 
     currency_id = fields.Many2one(
@@ -52,19 +57,42 @@ class Rma(models.Model):
     @api.model
     def default_get(self, default_fields):
         rec = super().default_get(default_fields)
-        active_id = self._context('active_id')
-        active_model = self._context.get('active_model')
-        if not active_id or active_model != 'sale.order':
+        active_id = self._context("active_id")
+        active_model = self._context.get("active_model")
+        if not active_id or active_model != "sale.order":
             return rec
 
-        sale_order = self.env['sale.order'].browse(active_id).filtered(lambda x: x.state not in ['cancel', 'draft'])
+        sale_order = (
+            self.env["sale.order"]
+            .browse(active_id)
+            .filtered(lambda x: x.state not in ["cancel", "draft"])
+        )
         if not sale_order:
-            raise UserError('Not Valid Order!')
+            raise UserError("Not Valid Order!")
 
-        rma = {
-            'sale_order_id': sale_order.id,
+        rma = {"sale_order_id": sale_order.id, "type": "return"}
 
-        }
+        return_amount = 0.0
+        return_lines = []
+        for line in sale_order.order_line:
+            return_lines.append(
+                (
+                    0,
+                    0,
+                    {
+                        "sale_line_id": line.id,
+                        "price_unit": line.price_unit,
+                        "product_uom": line.product_uom.id,
+                        "product_qty": line.product_uom_qty,
+                    },
+                )
+            )
+            return_amount += line.price_unit * lime.product_uom_qty
+
+        rma["return_amount"] = return_amount
+        rma["return_line_ids"] = return_lines
+        rec.update(rma)
+        return rec
 
 
 class RmaReturnLine(models.Model):
@@ -72,6 +100,25 @@ class RmaReturnLine(models.Model):
     _inherit = ["portal.mixin", "mail.thread", "mail.activity.mixin"]
     _description = "Sale Rma Return Line"
     _order = "id desc"
+
+    @api.depends("product_qty", "price_unit", "tax_id")
+    def _compute_amount(self):
+        for line in self:
+            taxes = line.tax_id.compute_all(
+                line.price_unit,
+                line.rma_id.currency_id,
+                line.product_qty,
+                partner=line.rma_id.sale_order_id.partner_shipping_id,
+            )
+            line.update(
+                {
+                    "price_tax": sum(
+                        t.get("amount", 0.0) for t in taxes.get("taxes", [])
+                    ),
+                    "price_total": taxes["total_included"],
+                    "price_subtotal": taxes["total_excluded"],
+                }
+            )
 
     rma_id = fields.Many2one("sale.rma", index=True)
     sale_line_id = fields.Many2one("sale.order.line", required=True)
@@ -85,8 +132,9 @@ class RmaReturnLine(models.Model):
         "Unit Price", required=True, digit="Product Price", default=0.0
     )
     price_subtotal = fields.Monetary(
-        compute="_compute_amount", string="Subtotal", readonly=True, store=True
+        compute="_compute_amount", string="Subtotal", store=True
     )
+    price_total = fields.Monetary(compute="_compute_amount", string="Total", store=True)
     product_qty = fields.Float(
         string="Quantity", digits="Product Unit Of Measure", required=True, default=1.0
     )
@@ -96,6 +144,7 @@ class RmaReturnLine(models.Model):
         domain="[('category_id', '=', product_uom_category_id)]",
     )
     currency_id = fields.Many2one("res.currency", related="rma_id.currency_id")
+    tax_id = fields.Many2many(related="sale_line_id.tax_id")
 
 
 class RmaExchangeLine(models.Model):
@@ -104,11 +153,28 @@ class RmaExchangeLine(models.Model):
     _description = "Sale Rma Exchange Line"
     _order = "id desc"
 
+    @api.depends("product_qty", "price_unit", "tax_id")
+    def _compute_amount(self):
+        for line in self:
+            taxes = line.tax_id.compute_all(
+                line.price_unit,
+                line.rma_id.currency_id,
+                line.product_qty,
+                partner=line.rma_id.sale_order_id.partner_shipping_id,
+            )
+            line.update(
+                {
+                    "price_tax": sum(
+                        t.get("amount", 0.0) for t in taxes.get("taxes", [])
+                    ),
+                    "price_total": taxes["total_included"],
+                    "price_subtotal": taxes["total_excluded"],
+                }
+            )
+
     rma_id = fields.Many2one("sale.rma", index=True)
     product_id = fields.Many2one("product.product", index=True)
-    product_uom_category_id = fields.Many2one(
-        "uom.category", compute="_compute_product_uom_category_id"
-    )
+    product_uom_category_id = fields.Many2one(related="product_id.uom_id.category_id")
     product_qty = fields.Float(
         string="Quantity", digits="Product Unit Of Measure", required=True, default=1.0
     )
@@ -122,6 +188,40 @@ class RmaExchangeLine(models.Model):
         "Unit Price", required=True, digit="Product Price", default=0.0
     )
     price_subtotal = fields.Monetary(
-        compute="_compute_amount", string="Subtotal", readonly=True, store=True
+        compute="_compute_amount", string="Subtotal", store=True
     )
+
+    price_total = fields.Monetary(compute="_compute_amount", string="Total", store=True)
     currency_id = fields.Many2one("res.currency", related="rma_id.currency_id")
+    tax_id = fields.Many2many(
+        "account.tax",
+        string="Taxes",
+        domain=["|", ("active", "=", False), ("active", "=", True)],
+    )
+
+    def _compute_tax_id(self):
+        for line in self:
+            fpos = (
+                line.rma_id.sale_order_id.fiscal_position_id
+                or line.sale_order_id.partner_id.property_account_position_id
+            )
+            taxes = line.product_id.taxes_id.filtered(
+                lambda r: not line.company_id or r.company_id == line.company_id
+            )
+            line.tax_id = (
+                fpos.map_tax(
+                    taxes,
+                    line.product_id,
+                    line.rma_id.sale_order_id.partner_shipping_id,
+                )
+                if fpos
+                else taxes
+            )
+
+    @api.onchange("product_id")
+    def product_id_change(self):
+        if not self.product_id:
+            return
+
+        self._compute_tax_id()
+        return {}
